052d5e436d7805fbcc321c83debf44848557c83a
RodYam <rodrigo.yoshio.yamamoto@gmail.com>
2023-10-02 10:43:26 -0300

    funções e listas

---
+++ js-curso-2-aula_-/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('#titulo');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrado = document.querySelector('p');
+paragrado.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute() {
+    console.log('O botao foi clicado');
+}
