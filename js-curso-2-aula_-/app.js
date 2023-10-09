5cc26fb8b4b06e6118930433c0eb0273ef975ecb
RodYam <rodrigo.yoshio.yamamoto@gmail.com>
2023-10-09 15:48:29 -0300

    Update app.js

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,9 +1,24 @@
+/*
 let titulo = document.querySelector('#titulo');
 titulo.innerHTML = 'Jogo do número secreto';
 
 let paragrado = document.querySelector('p');
 paragrado.innerHTML = 'Escolha um número entre 1 e 10';
+*/
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
 function verificarChute() {
-    console.log('O botao foi clicado');
+    console.log(numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
